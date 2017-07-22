7b096ae626b8ba36d09e7db4fde95489dfdd59c6
Gaelan Brewer <brewerg82@msn.com>
2017-07-22 13:29:05 -0700

    Updated the Nav fluent api calls

--- FeatureDemo.Core/Helpers/Nav.cs
+++ FeatureDemo.Core/Helpers/Nav.cs
@@ -6,59 +6,127 @@ using System.Linq;
 
 namespace FeatureDemo.Core.Helpers
 {
-    public class Nav : INav
+    public class Nav
     {
-
         public Nav()
         {
-            _to = new NavFluentInterface(this);
-            Paths = new List<string>();
+            Pages = new Dictionary<string, string>();
         }
+        private Dictionary<string, string> Pages { get; set; }
 
-        internal List<string> Paths;
-        private readonly NavFluentInterface _to;
-        public NavFluentInterface To 
-        { 
-            get 
-            { 
-                Paths.Clear(); 
-                return _to; 
-            } 
-        }
-    }
-
-    public class NavFluentInterface
-    {
-
-        private static Nav nav;
-
-        public NavFluentInterface(Nav _nav)
+        public NavFluentInterface To
         {
-            nav = _nav;
-        }
-
-        public NavFluentInterface GetPropName([CallerMemberName] string prop = "")
-        {
-            if (prop != "")
+            get
             {
-                nav.Paths.Add(prop);
+                Pages?.Clear();
+                return new NavFluentInterface(this);
             }
-            return this;
         }
 
-        public NavFluentInterface Root => GetPropName();
-        public NavFluentInterface Home => GetPropName();
-        public NavFluentInterface ItemDetail => GetPropName();
-        public NavFluentInterface Items => GetPropName();
-        public NavFluentInterface Menu => GetPropName();
-        public NavFluentInterface Navigation => GetPropName();
-        public NavFluentInterface Tabbed => GetPropName();
-        public NavFluentInterface About => GetPropName();
-        public NavFluentInterface NewItem => GetPropName();
-        public NavFluentInterface MasterDetail => GetPropName();
-        public NavFluentInterface Login => GetPropName();
+        private static string GetPropertyName([CallerMemberName] string name = "") => name;
 
-        public string Go => nav.Paths.ToArray().Aggregate((acc, item) => acc += $"{item}/").TrimEnd('/');
+        public static string Root => GetPropertyName();
+        public static string Home => GetPropertyName();
+        public static string ItemDetail => GetPropertyName();
+        public static string Items => GetPropertyName();
+        public static string Menu => GetPropertyName();
+        public static string Navigation => GetPropertyName();
+        public static string Tabbed => GetPropertyName();
+        public static string About => GetPropertyName();
+        public static string NewItem => GetPropertyName();
+        public static string MasterDetail => GetPropertyName();
+        public static string Login => GetPropertyName();
+
+        public class NavFluentInterface
+        {
+            private Nav _nav { get; set; }
+            public NavFluentInterface(Nav nav)
+            {
+                _nav = nav;
+            }
+
+            public NavFluentInterface Root(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.Root, parameters);
+                return this;
+            }
+
+            public NavFluentInterface Home(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.Home, parameters);
+                return this;
+            }
+
+            public NavFluentInterface ItemDetail(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.ItemDetail, parameters);
+                return this;
+            }
+
+            public NavFluentInterface Items(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.Items, parameters);
+                return this;
+            }
+
+            public NavFluentInterface Menu(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.Menu, parameters);
+                return this;
+            }
+
+            public NavFluentInterface Navigation(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.Navigation, parameters);
+                return this;
+            }
+
+            public NavFluentInterface Tabbed(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.Tabbed, parameters);
+                return this;
+            }
+
+            public NavFluentInterface About(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.About, parameters);
+                return this;
+            }
+
+            public NavFluentInterface NewItem(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.NewItem, parameters);
+                return this;
+            }
+
+            public NavFluentInterface MasterDetail(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.MasterDetail, parameters);
+                return this;
+            }
+
+            public NavFluentInterface Login(string parameters = null)
+            {
+                _nav.Pages.Add(Nav.Login, parameters);
+                return this;
+            }
+
+            public string Go
+            {
+                get
+                {
+                    if (_nav.Pages.Count == 0)
+                        return null;
+
+                    var path = new StringBuilder();
+                    foreach (var page in _nav.Pages)
+                    {
+                        path.Append(page.Value != null ? $"{page.Key}?{page.Value}/" : $"{page.Key}/");
+                    }
+
+                    return path.ToString().Trim('/');
+                }
+            }
+        }
     }
-
 }
