81ed242ff4ee2c361dee8e86ceeb873f1e1d4130
automachina <brewerg82@msn.com>
2017-08-02 06:56:25 -0700

    Added IGitHubApi.cs

---
+++ FeatureDemo.Core/Services/IGitHubApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using FeatureDemo.Core.Models;
+using Newtonsoft.Json;
+using RestEase;
+
+//https://github.com/canton7/RestEase
+namespace FeatureDemo.Core.Services
+{
+	[Header("User-Agent", "FeatureDemo")]
+	public interface IGitHubApi
+	{
+		[Get("users/{userId}")]
+		Task<User> GetUserAsync([Path] string userId);
+	}
+}
