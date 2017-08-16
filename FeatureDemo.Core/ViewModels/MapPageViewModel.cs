f4668a5a88303399bdedf35def70e26f86226d9f
automachina <brewerg82@msn.com>
2017-08-16 14:40:04 -0700

    Fixed Map Zoom with quadradic scaling.

--- FeatureDemo.Core/ViewModels/MapPageViewModel.cs
+++ FeatureDemo.Core/ViewModels/MapPageViewModel.cs
@@ -6,12 +6,15 @@ using TK.CustomMap;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using System.Diagnostics;
+using Prism.Commands;
 
 namespace FeatureDemo.Core.ViewModels
 {
     public class MapPageViewModel : BaseViewModel
     {
         Geo.IGeolocator geoloc;
+        public DelegateCommand<object> MapClickedCommand { get; set; }
+        public DelegateCommand<object> MapPropertyChangedCommand { get; set; }
 
         ObservableRangeCollection<TKCustomMapPin> _pins; public ObservableRangeCollection<TKCustomMapPin> Pins
         {
@@ -25,14 +28,42 @@ namespace FeatureDemo.Core.ViewModels
             set => SetProperty(ref _currentLocation, value);
         }
 
-        float _zoomLevel; public float ZoomLevel
+        double _zoomMin; public double ZoomMin
+        {
+            get => _zoomMin;
+            set => SetProperty(ref _zoomMin, value);
+        }
+
+		double _zoomMid; public double ZoomMid
+		{
+			get => _zoomMid;
+			set => SetProperty(ref _zoomMid, value);
+		}
+
+		double _zoomMax; public double ZoomMax
+		{
+			get => _zoomMax;
+			set => SetProperty(ref _zoomMax, value);
+		}
+
+        double _zoomLevel; public double ZoomLevel
         {
             get => _zoomLevel;
             set
             {
-                CurrentRegion = CurrentRegion?.WithZoom(value);
-                SetProperty(ref _zoomLevel, value);
+                if (value >= _zoomMin && value <= _zoomMax && CurrentRegion != null)
+                {
+                    ZoomValue = QuadradicZoom(value);
+                    CurrentRegion = MapSpan.FromCenterAndRadius(CurrentRegion.Center, Distance.FromKilometers(ZoomValue));
+					SetProperty(ref _zoomLevel, value);
+                }
             }
+        }
+
+        double _zoomValue; public double ZoomValue
+        {
+            get => _zoomValue;
+            set => SetProperty(ref _zoomValue, value);
         }
 
         MapSpan _currentRegion; public MapSpan CurrentRegion
@@ -41,11 +72,16 @@ namespace FeatureDemo.Core.ViewModels
             set => SetProperty(ref _currentRegion, value);
         }
 
+		MapSpan _visibleRegion; public MapSpan VisibleRegion
+		{
+			get => _visibleRegion;
+            set => SetProperty(ref _visibleRegion, value);
+		}
+
         public MapPageViewModel()
         {
             geoloc = CrossGeolocator.Current;
             geoloc.DesiredAccuracy = 100;
-
             Title = "ATM Map";
             Pins = new ObservableRangeCollection<TKCustomMapPin>(DataStore.GetAtmsAsync().Result);
             var geoPosition = new Geo.Position();
@@ -57,10 +93,35 @@ namespace FeatureDemo.Core.ViewModels
             {
                 geoPosition = new Geo.Position { Latitude = 44.066795, Longitude = -123.08019 };
             }
-
+            MapClickedCommand = new DelegateCommand<object>(MapClicked);
+            MapPropertyChangedCommand = new DelegateCommand<object>(MapPropertyChanged);
             CurrentLocation = new Position(geoPosition.Latitude, geoPosition.Longitude);
             CurrentRegion = new MapSpan(CurrentLocation, .08, .08);
+			ZoomMin = 0;
+            ZoomMid = 10;
+			ZoomMax = 500;
+			ZoomLevel = 0.5;
             Debug.WriteLine($"CurrentRegion.Radius: {CurrentRegion.Radius.Meters}");
+        }
+
+        public void MapClicked(object position)
+        {
+            if(position is Position)
+                CurrentRegion = MapSpan.FromCenterAndRadius((Position)position, CurrentRegion.Radius);
+        }
+
+        public void MapPropertyChanged(object map)
+        {
+            var prop = (TKCustomMap)map;
+        }
+
+		// Convert linear scale to quadradic scale
+		double QuadradicZoom(double value)
+        {
+			var a = (_zoomMin * _zoomMax - Math.Pow(_zoomMid, 2)) / (_zoomMin - 2 * _zoomMid + _zoomMax);
+			var b = Math.Pow((_zoomMid - _zoomMin), 2) / (_zoomMin - 2 * _zoomMid + _zoomMax);
+			var c = 2 * Math.Log((_zoomMax - _zoomMid) / (_zoomMid - _zoomMin));
+			return b * (Math.Exp(c * value) - 1.0);
         }
 
         public override void OnNavigatedTo(Prism.Navigation.NavigationParameters parameters)
